56d7b5300966170b82575cb602327e2c1bd01160
ACLEMUSM <aclemusm@unal.edu.co>
2020-02-26 22:37:06 -0500

    Add .gitignore file and database entity-relation model

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
 const mail = document.getElementById("mail");
 const submitFormButton = document.querySelector('button[type="submit"]');
 const password = document.getElementById("password");
-
-submitFormButton.addEventListener("submit", e => {
-  let errorMsg = [];
-  if (password.length >= 10) {
-    errorMsg.push("password is too short");
-  }
-  if (errorMsg.length > 0) {
-    e.preventDefault();
-    console.log("password is too short");
-  }
-});
